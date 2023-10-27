0ee8f09430b3b28f98b4242526ffb36404ffd235
Harish Ojha <132740226+Ojhaji000@users.noreply.github.com>
2023-10-27 22:44:08 +0530

    Create Super Reduced String.java
    https://www.hackerrank.com/challenges/reduced-string/problem

---
+++ Super Reduced String.java
@@ -0,0 +1,60 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+class Result {
+
+    /*
+     * Complete the 'superReducedString' function below.
+     *
+     * The function is expected to return a STRING.
+     * The function accepts STRING s as parameter.
+     */
+
+    public static String superReducedString(String s) {
+    // Write your code here
+        StringBuilder strB = new StringBuilder(s);
+        int i = 0;
+        while(i < strB.length()-1){
+            if(strB.charAt(i) == strB.charAt(i+1)){
+                strB.delete(i,i+2);
+                if(i != 0){
+                    i--;
+                }
+                continue;
+            }
+            i++;
+        }
+        String res = String.valueOf(strB);
+        if (res == ""){
+            res = "Empty String";
+        }
+        return res;
+    }
+
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        String s = bufferedReader.readLine();
+
+        String result = Result.superReducedString(s);
+
+        bufferedWriter.write(result);
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
